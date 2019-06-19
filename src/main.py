3279a63332753c9375030b14282908b9317f0f64
赵家庆 <zhaojiaqing@wondersgroup.com>
2019-06-19 14:14:24 +0800

    模型结构完善

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from src.model.SRGAN import SRGAN
+
+if __name__ == '__main__':
+    srGAN = SRGAN()
+    srGAN.train()
--- src/model/SRGAN.py
+++ src/model/SRGAN.py
@@ -12,6 +12,7 @@ import math
 
 
 def conv_block(inputs, filters, strides):
+
     """
     判别器的卷积块,顺序为conv+bn+leaky_relu
     参数：
@@ -27,6 +28,7 @@ def conv_block(inputs, filters, strides):
 
 
 def residual_block(inputs):
+
     """
     生成器的残差块
     """
@@ -68,7 +70,6 @@ class SRGAN(object):
         start_conv = PReLU(shared_axes=[1, 2])(start_conv)
 
         b = residual_block(start_conv)
-
         for _ in range(residual_count - 1):
             b = residual_block(b)
 
@@ -104,6 +105,7 @@ class SRGAN(object):
         out = Dense(1)(dense)
         model = Model(inputs, out)
 
+        # 判别器采用 wasserstein_loss
         model.compile(optimizer=keras.optimizers.RMSprop(2e-5), loss=self.wasserstein_loss)
 
         return model
@@ -111,7 +113,7 @@ class SRGAN(object):
     def build_vgg_model(self):
 
         img = Input(shape=[None, None, 3])
-        vgg = VGG19(include_top=False, weights='./vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5')
+        vgg = VGG19(include_top=False)
         vgg.outputs = [vgg.layers[20].output]
         vgg54 = Model(inputs=img, outputs=vgg(img))
         vgg54.trainable = False
@@ -122,28 +124,35 @@ class SRGAN(object):
 
         self.discriminator.trainable = False
 
+        # 真实图片
         valid_img = Input(shape=[None, None, 3])
 
+        # 尺寸裁剪图片
         fake_input = Input(shape=[None, None, 3])
+
+        # 生成器生成图片
         fake_img = self.generator(fake_input)
 
+        # vgg编码
         vgg_out1 = self.vgg_model(valid_img)
         vgg_out2 = self.vgg_model(fake_img)
 
+        # 计算vgg损失函数
         vgg_mse_loss = K.mean((vgg_out1 - vgg_out2) ** 2)
 
         model = Model([valid_img, fake_input], self.discriminator(fake_img))
 
+        # 损失函数替换为 vgg_loss + wasserstein_loss
         model.compile(keras.optimizers.RMSprop(2e-5),
-                      loss=lambda y_true, y_pred: vgg_mse_loss + 10e-3 * K.mean(y_true * y_pred))
+                      loss=lambda y_true, y_pred: vgg_mse_loss + 10e-3 * self.wasserstein_loss(y_true, y_pred))
 
         return model
 
-    def get_train_data():
+    def get_train_data(self, batch=1):
 
         files = None
         root = None
-        for _root, dirs, _files in os.walk('/home/notebooks/imagenet'):
+        for _root, dirs, _files in os.walk('../data'):
             files = _files
             root = _root
 
@@ -152,11 +161,14 @@ class SRGAN(object):
 
         for file in files:
 
+            # 将opencv 读取图片转换为 rgb
             img = cv2.imread(os.path.join(root, file))[:, :, [2, 1, 0]]
+
             shape = img.shape
             if shape[2] != 3:
                 continue
 
+            # 如果图片宽高不为偶数，转换为偶数，否则生成器生成图片和原始图片宽高不匹配
             if shape[1] % 2 != 0:
                 img = cv2.resize(img, (shape[1] - 1, shape[0]))
             if shape[0] % 2 != 0:
@@ -167,7 +179,7 @@ class SRGAN(object):
             x_train.append(img)
             x_resize.append(img_resize)
 
-            if len(x_train) == 1:
+            if len(x_train) == batch:
                 x_train = np.array(x_train) / (255 / 2) - 1
                 x_resize = np.array(x_resize) / (255 / 2) - 1
 
@@ -176,56 +188,54 @@ class SRGAN(object):
                 x_train = []
                 x_resize = []
 
+    def train(self):
 
-exit = 0
+        should_exit = 0
+        epochs = 2
+        log_path = '../data/train.log'
 
-if os.path.exists('./train.log'):
-    os.remove('./train.log')
+        # 将训练信息记录到日志
+        if os.path.exists(log_path):
+            os.remove(log_path)
 
-for t in range(4):
+        for t in range(epochs):
+            if should_exit == 1:
+                break
+            data_generator = self.get_train_data()
 
-    if exit == 1:
-        break
+            for i, (x_train, x_resize) in enumerate(data_generator):
 
-    data_generator = get_train_data()
+                try:
+                    shape = x_train.shape
 
-    for i, (x_train, x_resize) in enumerate(data_generator):
+                    valid = -np.ones([1, math.ceil(shape[1] / 16), math.ceil(shape[2] / 16), 1])
+                    fake = np.ones([1, math.ceil(shape[1] / 16), math.ceil(shape[2] / 16), 1])
 
-        try:
-            shape = x_train.shape
+                    combine_loss = self.model.train_on_batch([x_train, x_resize], valid)
 
-            valid = -np.ones([1, math.ceil(shape[1] / 16), math.ceil(shape[2] / 16), 1])
-            fake = np.ones([1, math.ceil(shape[1] / 16), math.ceil(shape[2] / 16), 1])
+                    generate_imgs = self.generator.predict(x_resize)
+                    d_loss_valid = self.discriminator.train_on_batch(x_train, valid)
+                    d_loss_fake = self.discriminator.train_on_batch(generate_imgs, fake)
 
-            combine_loss = srGAN.model.train_on_batch([x_train, x_resize], valid)
+                    # wgan权重裁剪
+                    for l in self.discriminator.layers:
+                        weights = l.get_weights()
+                        weights = [np.clip(w, -0.01, 0.01) for w in weights]
+                        l.set_weights(weights)
 
-            generate_imgs = srGAN.generator.predict(x_resize)
-            d_loss_valid = srGAN.discriminator.train_on_batch(x_train, valid)
-            d_loss_fake = srGAN.discriminator.train_on_batch(generate_imgs, fake)
+                    with open(log_path, 'a', encoding='utf-8') as file:
+                        file.write(f'epoch {t} 第{i}个batch,生成器损失： {combine_loss} 判别器损失: {(d_loss_valid + d_loss_fake) / 2}\n')
 
-            # 权重裁剪
-            for l in srGAN.discriminator.layers:
-                weights = l.get_weights()
-                weights = [np.clip(w, -0.01, 0.01) for w in weights]
-                l.set_weights(weights)
+                    # 每 2000 保存一次生成样本
+                    if i % 2000 == 0:
+                        generate_imgs = ((generate_imgs[0] + 1) * (255 / 2))[:, :, [2, 1, 0]]
+                        cv2.imwrite(f'./image/{t}_{i}.jpg', generate_imgs)
 
-            with open('/home/notebooks/train.log', 'a', encoding='utf-8') as file:
-                file.write('大循环' + str(t) + '第' + str(i) + '次 训练, 生成器损失：' + str(combine_loss) + ' 判别器损失: ' + str(
-                    (d_loss_valid + d_loss_fake) / 2) + '\n')
+                    # 每训练1000个batch保存权重
+                    if i % 1000 == 0:
+                        self.discriminator.save_weights('../weight/discriminator_{i}.w')
+                        self.generator.save_weights('../weight/generator_{i}.w')
 
-            if i % 2000 == 0:
-                generate_imgs = ((generate_imgs[0] + 1) * (255 / 2))[:, :, [2, 1, 0]]
-                cv2.imwrite('./image/' + str(t) + '_' + str(i) + '.jpg', generate_imgs)
-
-            if i % 1000 == 0:
-                srGAN.discriminator.save_weights('./weight/discriminator_' + str(i) + '.w')
-                srGAN.generator.save_weights('./weight/generator_' + str(i) + '.w')
-
-        except KeyboardInterrupt as e:
-            print('手动停止')
-            exit = 1
-            break
-
-        except:
-            with open('/home/notebooks/train.log', 'a', encoding='utf-8') as file:
-                file.write('第' + str(i) + '条数据太大跳过\n')
+                except:
+                    with open(log_path, 'a', encoding='utf-8') as file:
+                        file.write('第{i}batch数据太大跳过\n')
