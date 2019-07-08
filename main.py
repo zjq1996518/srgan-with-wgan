4cf753f1a45ee4089ccd69b0081f46b56e5b37b1
赵家庆 <zhaojiaqing@wondersgroup.com>
2019-07-08 17:04:17 +0800

    修改环境问题

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from src.model.SRGAN import SRGAN
+import sys
+import os
 
 if __name__ == '__main__':
+
+    # 读取根目录绝对路径
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    root_path = os.path.split(cur_path)[0]
+    sys.path.append(root_path)
+
     srGAN = SRGAN()
     srGAN.train()
